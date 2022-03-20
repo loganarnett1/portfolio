f51ff87af1576464a218c071fa6897899ab59b0d
loganarnett1 <loganarnett@gmail.com>
2022-03-19 22:14:46 -0400

    started second section

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,30 @@
 var frameNumber = 0,
-    playbackConst = 500,   
+    playbackConst = 550,   
     vid = document.getElementById('topVideo'),
     frameCount = 40; 
+    framerate = 24;
+
+let topVideo = document.getElementById(`topVideo`);
+let topVideoContainer = document.getElementById(`mainVideoContainer`);
+let topVideoPadding = document.getElementById(`topVideoPadding`)
+
 
 function scrollPlay(){  
-  //var frameNumber  = window.pageYOffset/playbackConst;
-  var frameNumber  = window.pageYOffset/window.innerHeight;
+  var frameNumber  = (window.pageYOffset/window.innerHeight) * (frameCount / framerate);
   vid.currentTime  = frameNumber;
   window.requestAnimationFrame(scrollPlay);
 }
 
 window.requestAnimationFrame(scrollPlay);
+
+function updatePadding(){
+  let height = topVideoContainer.offsetHeight;
+  topVideoPadding.style.paddingBottom = `${height}px`;
+  console.log(`test ${height}`);
+}
+
+window.onresize = updatePadding;
+
+updatePadding();
+
 
